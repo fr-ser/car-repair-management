74e6cd3840133b484b810d61feba25d0dba46c14
Sergej Herbert <sergej.herbert@sennder.com>
2025-09-01 19:37:46 +0200

    feat: finalize user model

--- backend/test/helpers.ts
+++ backend/test/helpers.ts
@@ -1,10 +1,8 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as argon from 'argon2';
 
 import { AppModule } from 'src/app.module';
-import { AuthService } from 'src/auth/auth.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 export async function resetDatabase(prisma: PrismaService) {
@@ -37,20 +35,4 @@ export async function createTestClientApp(): Promise<
   await app.listen(configService.getOrThrow<number>('PORT'));
 
   return [moduleRef, app];
-}
-
-export async function createUser(
-  app: INestApplication,
-  { email = 'test@test.test', password = '123' },
-): Promise<string> {
-  const prisma = app.get(PrismaService);
-  const user = await prisma.user.create({
-    data: {
-      email,
-      hash: await argon.hash(password),
-    },
-  });
-
-  const auth = app.get(AuthService);
-  return await auth.getSignedToken(user.id, user.email);
 }
