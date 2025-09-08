65775b6dd1b83c15c81ff4bd663b941c9ae3025b
NikitaZavartsev <nikoss9644@gmail.com>
2025-09-08 19:19:50 +0200

    test: add clients to seed dev and test envs

--- backend/prisma/seed/dev.ts
+++ backend/prisma/seed/dev.ts
@@ -17,6 +17,76 @@ const seed = async () => {
       { id: 'foo2', description: 'bar2', price: '2.4', amount: '293' },
     ],
   });
+  await prisma.client.deleteMany();
+  await prisma.client.createMany({
+    data: [
+      // order matters because we want to see the results ordered by createdAt,
+      // so the newest are on the very top of the list
+      {
+        clientNumber: 'K7',
+        firstName: 'Ethan',
+        lastName: 'Wilson',
+        postalCode: '12345',
+        city: 'Sample City',
+        street: 'Main St',
+        email: 'ethan.wilson@example.com',
+      },
+      {
+        clientNumber: 'K6',
+        firstName: 'Diana',
+        lastName: 'Miller',
+        postalCode: '12345',
+        city: 'Sample City',
+        street: 'Main St',
+        email: 'diana.miller@example.com',
+      },
+      {
+        clientNumber: 'K5',
+        firstName: 'Charlie',
+        lastName: 'Davis',
+        postalCode: '12345',
+        city: 'Sample City',
+        street: 'Main St',
+        email: 'charlie.davis@example.com',
+      },
+      {
+        clientNumber: 'K4',
+        firstName: 'Bob',
+        lastName: 'Brown',
+        postalCode: '12345',
+        city: 'Sample City',
+        street: 'Main St',
+        email: 'bob.brown@example.com',
+      },
+      {
+        clientNumber: 'K3',
+        firstName: 'Alice',
+        lastName: 'Johnson',
+        postalCode: '12345',
+        city: 'Sample City',
+        street: 'Main St',
+        email: 'alice.johnson@example.com',
+      },
+      {
+        clientNumber: 'K2',
+        firstName: 'Jane',
+        lastName: 'Smith',
+        postalCode: '12345',
+        city: 'Sample City',
+        street: 'Main St',
+        email: 'jane.smith@example.com',
+      },
+      {
+        clientNumber: 'K1',
+        firstName: 'John',
+        lastName: 'Doe',
+        postalCode: '12345',
+        city: 'Sample City',
+        street: 'Main St',
+        email: 'john.doe@example.com',
+      },
+    ],
+  });
 };
 
 seed().catch(console.error);
--- backend/prisma/seed/playwright.ts
+++ backend/prisma/seed/playwright.ts
@@ -9,6 +9,76 @@ const seed = async () => {
   await prisma.user.create({
     data: { userName: 'test-user', hash: await argon.hash('test-pass') },
   });
+  await prisma.client.deleteMany();
+  await prisma.client.createMany({
+    data: [
+      // order matters because we want to see the results ordered by createdAt,
+      // so the newest are on the very top of the list
+      {
+        clientNumber: 'K7',
+        firstName: 'FTest 7',
+        lastName: 'LTest 7',
+        postalCode: '77777',
+        city: 'test City',
+        street: 'teststr. 7',
+        email: 'test.7@example.com',
+      },
+      {
+        clientNumber: 'K6',
+        firstName: 'FTest 6',
+        lastName: 'LTest 6',
+        postalCode: '66666',
+        city: 'test City',
+        street: 'teststr. 6',
+        email: 'test.6@example.com',
+      },
+      {
+        clientNumber: 'K5',
+        firstName: 'FTest 5',
+        lastName: 'LTest 5',
+        postalCode: '55555',
+        city: 'test City',
+        street: 'teststr. 5',
+        email: 'test.5@example.com',
+      },
+      {
+        clientNumber: 'K4',
+        firstName: 'FTest 4',
+        lastName: 'LTest 4',
+        postalCode: '44444',
+        city: 'test City',
+        street: 'teststr. 4',
+        email: 'test.4@example.com',
+      },
+      {
+        clientNumber: 'K3',
+        firstName: 'FTest 3',
+        lastName: 'LTest 3',
+        postalCode: '33333',
+        city: 'test City',
+        street: 'teststr. 3',
+        email: 'test.3@example.com',
+      },
+      {
+        clientNumber: 'K2',
+        firstName: 'FTest 2',
+        lastName: 'LTest 2',
+        postalCode: '22222',
+        city: 'test City',
+        street: 'teststr. 2',
+        email: 'test.2@example.com',
+      },
+      {
+        clientNumber: 'K1',
+        firstName: 'FTest 1',
+        lastName: 'LTest 1',
+        postalCode: '11111',
+        city: 'test City',
+        street: 'teststr. 1',
+        email: 'test.1@example.com',
+      },
+    ],
+  });
 };
 
 seed().catch(console.error);
