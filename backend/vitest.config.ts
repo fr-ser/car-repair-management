651429c1f220a83328f5456c5e63a446773d56c5
Sergej Herbert <sergej.herbert@not-real-git.com>
2026-04-11 17:32:08 +0200

    chore: no test logging

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    setupFiles: ['src/test-setup.ts'],
     include: ['**/*.{spec,e2e-spec}.[jt]s'],
     environment: 'node',
     clearMocks: true,
